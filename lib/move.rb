fb50fe7db484e8ad873a3d44bd4d8b90b18e913a
sofia tofigh <sst2140@columbia.edu>
2020-02-04 01:10:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,5 @@ def display_board(board)
 end
 
 def input_to_index(input = ["1".to_i, "2".to_i, "3".to_i, "4".to_i, "5".to_i, "6".to_i, "7".to_i, "8".to_i, "9".to_i])
-  puts '1'.to_i, '2'.to_i,'3'.to_i, '4'.to_i,'5'.to_i, '6'.to_i,'7'.to_i, '8'.to_i,'9'.to_i
   
 end
